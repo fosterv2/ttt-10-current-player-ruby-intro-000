702fb67b23f034499144b91fb3cf5ad59c852721
Valerie Foster <fosterv2@outlook.com>
2019-11-20 23:48:59 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 == 0
+    "O"
+  else
+    "X"
+  end
+end
